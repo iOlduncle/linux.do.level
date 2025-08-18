580364ee652292a4e1b182c3e35163ac08dc092b
IO <8361027+iOlduncle@users.noreply.github.com>
2025-08-18 14:39:00 +0800

    修复楼层显示的样式相关问题

--- src/floor/floor-ui.ts
+++ src/floor/floor-ui.ts
@@ -1,6 +1,6 @@
 export function createFloor(num: string) {
     let button = document.createElement('button');
-    button.className ='widget-button btn-flat reply create fade-out btn-icon-text';
+    button.className ='btn no-text btn-icon btn-flat';
     button.setAttribute('title',`${num}楼`);
     button.setAttribute('id','floor-button');
     button.innerHTML = `<span class='d-button-label floor-text'>#${num}</span>`;
